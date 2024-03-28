a03aeae8d646c0e93664ec0a3e1850100d2466e8
Sharhan123 <sharhanmohammed03@gmail.com>
2024-03-28 14:30:45 +0530

    trying more

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosWithAuth = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: "https://dns-manager-api-kw7o-git-main-sharhan123s-projects.vercel.app",
 });
 
 axiosWithAuth.interceptors.request.use(
