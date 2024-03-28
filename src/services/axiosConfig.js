d2da8f1e77140470858498daf987cb7be7cdf59d
Sharhan123 <sharhanmohammed03@gmail.com>
2024-03-28 16:21:17 +0530

    commiting

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosWithAuth = axios.create({
-  baseURL: "https://api-m11bxfu2t-sharhan123s-projects.vercel.app",
+  baseURL: "https://api-dns-record.vercel.app/",
 });
 
 axiosWithAuth.interceptors.request.use(
